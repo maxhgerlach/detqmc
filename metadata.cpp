7ab3526e38aaa603f5dac1c4f208050de22cccb6
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2012-12-12 19:54:35 +0100

    metadata, changed code from dlib to boost::property_tree

---
+++ metadata.cpp
@@ -0,0 +1,96 @@
+//Code by Max Henner Gerlach, 2010--2012,
+//used for the diploma thesis "Directional Ordering in the Classical Compass Model in Two and Three Dimensions"
+//contact: maxgerlach@gmail.com
+
+/*
+ * metadata.cpp
+ *
+ *  Created on: Apr 20, 2011
+ *      Author: gerlach
+ */
+
+#include "metadata.h"
+#include <boost/property_tree/ptree.hpp>      //config file parsing
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/algorithm/string.hpp>   //trimming
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+//parse metadata from a configfile-like section of lines of the form
+//key = value
+//into a map string->string
+MetadataMap parseMetadataBlock(std::string& lines) {
+	namespace bpt = boost::property_tree;
+	bpt::ptree pt;
+    std::istringstream linesStream(lines);
+    bpt::ini_parser::read_ini(linesStream, pt);
+    MetadataMap meta;
+    for (bpt::ptree::const_iterator p = pt.begin(); p != pt.end(); ++p) {
+    	meta[p->first] = p->second.data();
+    }
+
+
+
+
+//    typedef dlib::config_reader::kernel_1a CR;
+//    CR cr(linesStream);
+//    std::vector<string> keys;
+//    cr.get_keys(keys);
+//    for (unsigned i = 0; i < keys.size(); ++i)
+//        meta[keys[i]] = cr[keys[i]];
+//    return meta;
+
+    return meta;
+}
+
+std::string metadataToString(const MetadataMap& meta, const char* leading) {
+    std::string result;
+    MetadataMap::const_iterator p;
+    for (p = meta.begin(); p != meta.end(); ++p) {
+        result += leading + p->first + " = " + p->second + "\n";
+    }
+    return result;
+}
+
+MetadataMap readOnlyMetadata(const std::string & filename) {
+    std::ifstream inFile(filename.c_str());
+    std::string collectedLines;
+    std::string line;
+    bool done = false;
+    do {
+        std::getline(inFile, line);
+        boost::algorithm::trim_left(line);
+        if (line[0] == '#') {
+            line[0] = ' ';
+            collectedLines += line + '\n';
+        } else {
+            done = true;
+        }
+    } while (inFile and not done);
+    return parseMetadataBlock(collectedLines);
+}
+
+void writeOnlyMetaData(const std::string& filename, const MetadataMap& meta,
+        const std::string& leadingCommentsBlock) {
+    std::string comments = leadingCommentsBlock;
+    //add "##" characters
+    comments.insert(0, "##");
+    size_t found = comments.find_first_of('\n');
+    while (found != std::string::npos) {
+        comments.insert(found + 1, "##");
+        found = comments.find_first_of('\n', found + 1);
+//        if (found + 1 == comments.length()) {
+//            //don't add "##" after the last line
+//            break;
+//        }
+    }
+    if (*comments.rbegin() != '\n') {
+        comments.append("\n");
+    }
+    std::ofstream outFile(filename.c_str());
+    outFile << comments;
+    outFile << metadataToString(meta, "#");
+}
+
+
---
+++ metadata.h
@@ -0,0 +1,43 @@
+//Code by Max Henner Gerlach, 2010--2012,
+//used for the diploma thesis "Directional Ordering in the Classical Compass Model in Two and Three Dimensions"
+//contact: maxgerlach@gmail.com
+
+/*
+ * metadata.h
+ *
+ *  Created on: Apr 20, 2011
+ *      Author: gerlach
+ */
+
+#ifndef METADATA_H_
+#define METADATA_H_
+
+#include <map>
+#include <string>
+
+typedef std::map<std::string, std::string> MetadataMap;
+
+//parse metadata from a configfile-like section of lines of the form
+//key = value
+//into a map string->string
+MetadataMap parseMetadataBlock(std::string& lines);
+
+//open the file and read only its leading comment block, parse that and
+//return the metadata
+MetadataMap readOnlyMetadata(const std::string& filename);
+
+//write out metadata to a string in the form
+//[leading] key = value
+//with [leading] = "#" for instance
+std::string metadataToString(const MetadataMap& meta,
+        const char* leading = "");
+
+//write out metadata to file, with leading "#" characters;
+//additional block of comment lines gets written out first with "##" added
+//before each line
+void writeOnlyMetaData(const std::string& filename, const MetadataMap& meta,
+        const std::string& leadingCommentsBlock = "");
+
+
+
+#endif /* METADATA_H_ */
