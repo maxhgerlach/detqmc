8830f41e4109c949bfdfa0c91d828585b84b411e
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2013-04-17 19:38:15 +0200

    serialization now specified explicitly for std::vector<std::unique_ptr<T>> to use std::move

--- boost_serialize_vector_uniqueptr.h
+++ boost_serialize_vector_uniqueptr.h
@@ -8,19 +8,22 @@
 #ifndef BOOST_SERIALIZE_VECTOR_UNIQUEPTR_H_
 #define BOOST_SERIALIZE_VECTOR_UNIQUEPTR_H_
 
-//Boost serialization for std::vector<std::unique_ptr<T>>, following
+//Boost serialization for std::vector<std::unique_ptr<T>>, based on
 // http://stackoverflow.com/questions/13347776/boost-serialization-of-stl-collection-of-std-unique-ptrs
 
-//NOTE: do not include boost/serialization/vector.hpp
+//NOTE: do not include boost/serialization/vector.hpp //is this note still relevant?
 
+#include <memory>
+#include <vector>
 #include <boost/serialization/nvp.hpp>
 
 namespace boost { namespace serialization {
 
 
 template<class Archive, class T, class Allocator>
-inline void save(Archive& ar, const std::vector<T, Allocator>& vec,
-				 const unsigned int) {
+inline void save(Archive& ar,
+				 const std::vector<std::unique_ptr<T>, Allocator>& vec,
+				 const unsigned int /*version*/) {
 	collection_size_type count = vec.size();
 	ar << BOOST_SERIALIZATION_NVP(count);
 	for (auto it = vec.begin(), end = vec.end(); it != end; ++it)
@@ -28,24 +31,27 @@ inline void save(Archive& ar, const std::vector<T, Allocator>& vec,
 }
 
 template<class Archive, class T, class Allocator>
-inline void load(Archive& ar, std::vector<T, Allocator>& vec,
-				 const unsigned int) {
+inline void load(Archive& ar,
+				 std::vector<std::unique_ptr<T>, Allocator>& vec,
+				 const unsigned int /*version*/) {
     collection_size_type count;
     ar >> BOOST_SERIALIZATION_NVP(count);
     vec.clear();
     vec.reserve(count);
     while( count-- > 0 ) {
-        T i;
+        std::unique_ptr<T> i;
         ar >> boost::serialization::make_nvp("item", i);
         vec.push_back(std::move(i)); // use std::move
     }
 }
 
 template<class Archive, class T, class Allocator>
-inline void serialize(Archive& ar, std::vector<T, Allocator>& t,
+inline void serialize(Archive& ar,
+					  std::vector<std::unique_ptr<T>, Allocator>& t,
 					  const unsigned int file_version) {
     boost::serialization::split_free(ar, t, file_version);
 }
+
 
 } } // namespace boost::serialization
 
