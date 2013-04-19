074d3ff7ef3b314e6262034dc3b23ab01cadffe5
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2013-04-19 13:21:19 +0200

    added autocorrelation time estimators to statistics.h

--- statistics.h
+++ statistics.h
@@ -68,6 +68,126 @@ std::tuple<T,T> jackknife(const std::vector<T>& blockValues, const T& zeroValue 
 }
 
 
+//if end==0: compute average over whole vector
+//else compute average for elements at start, start+1, ..., end-1
+template<typename T>
+T average(const std::vector<T>& vec, std::size_t start = 0, std::size_t end = 0) {
+    if (end==0) {
+        end = vec.size();
+    }
+//  assert(end > start);
+    T avg = T(0);
+    for (std::size_t i = start; i < end; ++i) {
+        avg += T(vec[i]);
+    }
+    avg /= (end-start);
+    return avg;
+}
+
+
+
+//integrated autocorrelation time,
+//employ a self-consistent cut-off
+//\tau_int = 1/2 + \sum_{k=1}^{k_max} A(k)
+//k_max \approx 6*\tau_int
+template<typename T>
+T tauint(const std::vector<T>& data, T selfConsCutOff = T(6)) {
+    int m = data.size();
+    T mean = average(data);
+    T var = 0;
+    T result = 0.5;
+    for (std::size_t t = 0; t < m - 1; ++t) {
+        T autoCorr = 0;
+        for (size_t k = 0; k < m - t; ++k){
+            autoCorr += (data[k] - mean) * (data[k + t] - mean);
+        }
+        autoCorr /= (m - t);
+        if (t == 0) {
+            var = autoCorr;
+        } else {
+            result += autoCorr / var;
+            if (t > selfConsCutOff * result)
+                break;
+        }
+    }
+    return result;
+}
+
+//integrated autocorrelation time,
+//stop accumulating once autoCorr <= 0
+template<typename T>
+T tauint_stopAtZeroCrossing(const std::vector<T>& data) {
+    std::size_t m = data.size();
+    T mean = average(data);
+    T var = 0;
+    T result = 0.5;
+    for (int t = 0; t < m - 1; ++t) {
+        T autoCorr = 0;
+        for (int k = 0; k < m - t; ++k){
+            autoCorr += (data[k] - mean) * (data[k + t] - mean);
+        }
+        autoCorr /= (m - t);
+        if (t == 0) {
+            var = autoCorr;
+        } else {
+            if (autoCorr <= 0) {
+                break;
+            } else {
+                result += autoCorr / var;
+            }
+        }
+    }
+    return result;
+}
+
+//faster estimation of tauint using an adaptive integration scheme (compare [Chodera2007] pg. 38)
+//(also stops at the zero crossing of autoCorr)
+template<typename T>
+T tauint_adaptive(const std::vector<T>& data) {
+    std::size_t m = data.size();
+    T mean = average(data);
+    T result = 0.5;
+
+    //compute variance
+    T var = 0;
+    for (size_t k = 0; k < m; ++k){
+        var += (data[k] - mean) * (data[k] - mean);
+    }
+    var /= m;
+
+    //adaptive integration of autocorrelation function
+    //high time resolution for small lag times, lower resolution for higher times in the
+    //slowly decaying tail of the autocorrelation function
+    size_t i = 1;
+    size_t t_i = 1;
+    while (t_i < m - 1) {
+        //lag time for this step of the iteration
+        t_i = 1 + i * (i - 1) / 2;
+
+        //compute autocorrelation function
+        T autoCorr = 0.0;
+        for (size_t k = 0; k < m - t_i; ++k){
+            autoCorr += (data[k] - mean) * (data[k + t_i] - mean);
+        }
+        autoCorr /= (m - t_i);
+        autoCorr /= var;
+
+        if (autoCorr <= 0) {
+            break;
+        } else {
+            //weighted addition to estimate integrated autocorrelation time
+            T t_next = 1 + (i+1) * (i) / 2;
+            result += autoCorr * (t_next - t_i);
+        }
+        i = i + 1;
+    }
+
+    return result;
+}
+
+
+
+
 
 
 
