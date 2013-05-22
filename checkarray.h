64c8a030e25f8379d6bd691a77ca29c467898f26
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2013-05-22 14:08:27 +0200

    Added checkarray: a std::array with bounds checking operator[] if the code is compiled with -D_GLIBCXX_DEBUG

---
+++ checkarray.h
@@ -0,0 +1,34 @@
+/*
+ * checkarray.h
+ *
+ *  Created on: May 22, 2013
+ *      Author: gerlach
+ */
+
+#ifndef CHECKARRAY_H_
+#define CHECKARRAY_H_
+
+// a variation of std::array that includes bounds checking for
+// operator[] if the compilation flag -D_GLIBCXX_DEBUG is given (like
+// std::vector), compare to:
+// http://stackoverflow.com/questions/8181887/bound-checking-of-stdarray-in-debug-version-of-gcc
+
+#include <array>
+
+template<typename T, std::size_t N>
+struct checkarray : std::array<T,N> {
+#ifdef _GLIBCXX_DEBUG
+	reference operator[](size_type n)	{
+		return at(n);
+	}
+
+	const_reference	operator[](size_type n) const	{
+		return at(n);
+	}
+#endif //_GLIBCXX_DEBUG
+};
+
+
+
+
+#endif /* CHECKARRAY_H_ */
