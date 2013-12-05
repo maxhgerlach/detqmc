0bd8c92c227c4dae3a41e71ce0eff0b815d3a171
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2013-12-05 17:17:01 +0100

    work on maindetsdw-checkerboard-compare

--- maindetsdw-checkerboard-compare.cpp
+++ maindetsdw-checkerboard-compare.cpp
@@ -19,22 +19,6 @@
 #include "rngwrapper.h"
 
 
-// class derived from DetSDW that only adds a read-only interface to the Green's
-// function
-// // this makes thinks horribly hard
-//template <bool CheckerBoard>
-//class DetSDW_getGreen : public DetSDW<false, CheckerBoard> {
-//public:
-//	typedef DetSDW<false, CheckerBoard> Base;
-//	DetSDW_getGreen(const Base& base) : Base(base) {
-//	}
-//	const CubeCpx& get_g() {
-//		return g;
-//	}
-//private:
-//	using Base::g;
-//};
-
 
 
 ModelParams setupParameters(bool checkerboard_, uint32_t m_, num dtau_, uint32_t s_, std::string bc_) {
@@ -70,14 +54,17 @@ ModelParams setupParameters(bool checkerboard_, uint32_t m_, num dtau_, uint32_t
 
 int main() {
 	std::string bc = "pbc";
-	std::vector<num> dtau_values = {0.01, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5};
+//	std::vector<num> dtau_values = {0.01, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5};
+	std::vector<num> dtau_values = {0.5, 0.4, 0.3, 0.2, 0.15, 0.1, 0.05, 0.01};
 	num beta = 10;
-	uint32_t s = 10;
+	uint32_t s = 1;
 	std::vector<uint32_t> m_values;
 	m_values.reserve(dtau_values.size());
 	for (num dtau : dtau_values) {
 		m_values.push_back(std::ceil(beta / dtau));
 	}
+
+	std::cout << "dtau\tAbsMin\tAbsMax\tAbsMean\tRelMin\tRelMax\tRelMean\t" << std::endl;
 
 	for (uint32_t i = 0; i < dtau_values.size(); ++i) {
 		num dtau = dtau_values[i];
@@ -87,13 +74,13 @@ int main() {
 		// same parameters and same rng seed
 
 		ModelParams pars_cb = setupParameters(true, m, dtau, s, bc);
-		RngWrapper rng_cb(1111);
+		RngWrapper rng_cb(5555);
 		typedef DetSDW<false, true> Cb;
 		std::unique_ptr<DetModel> cb_model_ptr = createDetSDW(rng_cb, pars_cb);
 		std::unique_ptr<Cb> sdw_cb = std::unique_ptr<Cb>(dynamic_cast<Cb*>(cb_model_ptr.release()));
 
 		ModelParams pars_reg = setupParameters(false, m, dtau, s, bc);
-		RngWrapper rng_reg(1111);
+		RngWrapper rng_reg(5555);
 		typedef DetSDW<false, false> Reg;
 		std::unique_ptr<DetModel> reg_model_ptr = createDetSDW(rng_reg, pars_reg);
 		std::unique_ptr<Reg> sdw_reg = std::unique_ptr<Reg>(dynamic_cast<Reg*>(reg_model_ptr.release()));
@@ -108,22 +95,31 @@ int main() {
 		// compute differences of the Green's functions
 
 		CubeNum g_abs_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green());
-		//CubeNum g_abs_diff = arma::abs(sdw_reg->get_green());
-		//CubeNum g_abs_diff = arma::real(sdw_reg->get_green());
 		num max_abs_diff = g_abs_diff.max();
+		num min_abs_diff = g_abs_diff.min();
 		num mean_abs_diff = 0.0;
+		CubeNum g_rel_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green()) / arma::abs(sdw_reg->get_green());
+		num max_rel_diff = g_rel_diff.max();
+		num min_rel_diff = g_rel_diff.min();
+		num mean_rel_diff = 0.0;
 		for (uint32_t k = 1; k <= m; ++k) {
-			num mean_k = 0.0;
+			num mean_abs_k = 0.0;
+			num mean_rel_k = 0.0;
 			uint32_t elems = g_abs_diff.slice(k).size();
 			for (uint32_t i = 0; i < elems; ++i) {
-				mean_k += g_abs_diff.slice(k)[i];
+				mean_abs_k += g_abs_diff.slice(k)[i];
+				mean_rel_k += g_rel_diff.slice(k)[i];
 			}
-			mean_k /= num(elems);
-			mean_abs_diff += mean_k;
+			mean_abs_k /= num(elems);
+			mean_abs_diff += mean_abs_k;
+			mean_rel_k /= num(elems);
+			mean_rel_diff += mean_rel_k;
 		}
 		mean_abs_diff /= num(m);
+		mean_rel_diff /= num(m);
 
-		std::cout << dtau << '\t' << max_abs_diff << '\t' << mean_abs_diff << std::endl;
+		std::cout << dtau << '\t' << min_abs_diff << '\t' << max_abs_diff << '\t' << mean_abs_diff;
+		std::cout         << '\t' << min_rel_diff << '\t' << max_rel_diff << '\t' << mean_rel_diff << std::endl;
 	}
 
 	return 0;
