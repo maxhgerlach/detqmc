edfdb5bc26a9829d4379012a45c79c84fa1e9aa0
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2013-12-17 16:19:18 +0100

    maindetsdw-checkerboard-compare work

--- maindetsdw-checkerboard-compare.cpp
+++ maindetsdw-checkerboard-compare.cpp
@@ -56,14 +56,14 @@ ModelParams setupParameters(bool checkerboard_, uint32_t m_, num dtau_, uint32_t
 int main() {
 	std::vector<std::string> bc_values = {"pbc", "apbc-x", "apbc-y", "apbc-xy"};
 //	std::vector<num> dtau_values = {0.01, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5};
-//	std::vector<num> dtau_values = {0.5, 0.4, 0.3, 0.2, 0.15, 0.1, 0.05, 0.01};
+	std::vector<num> dtau_values = {0.5, 0.4, 0.3, 0.2, 0.15, 0.1, 0.05, 0.01};
 	//std::vector<std::string> bc_values = {"apbc-x"};
 	//std::vector<std::string> bc_values = {"pbc"};
 	//std::vector<std::string> bc_values = {"apbc-x", "pbc"};
 //	std::vector<num> dtau_values = {0.5, 0.4, 0.3, 0.2, 0.15};
-	std::vector<num> dtau_values = {0.1};
-	std::vector<uint32_t> s_values = {10};
-	//std::vector<uint32_t> s_values = {10};
+	//std::vector<num> dtau_values = {0.1};
+	std::vector<uint32_t> s_values = {10, 1};
+	std::vector<uint32_t> L_values = {4, 6, 8, 10};
 	num beta = 10;
 	std::vector<uint32_t> m_values;
 	m_values.reserve(dtau_values.size());
@@ -75,72 +75,74 @@ int main() {
 			<< setupParameters(true, 10, 0.1, 1, "pbc").checkerboardMethod
 			<< "\n"
 			<< std::endl;
-	for (bool shiftGreen : {true, false}) {
-		for (auto bc : bc_values) {
-			for (auto s : s_values) {
-				std::cout << bc << ", s = " << s << " shiftGreen: " << shiftGreen << std::endl;
-				std::cout << "dtau\tAbsMin\tAbsMax\tAbsMean\tRelMin\tRelMax\tRelMean\t" << std::endl;
-				for (uint32_t i = 0; i < dtau_values.size(); ++i) {
-					num dtau = dtau_values[i];
-					uint32_t m = m_values[i];
+	for (auto L : L_values) {
+		for (bool shiftGreen : {true, false}) {
+			for (auto bc : bc_values) {
+				for (auto s : s_values) {
+					std::cout << "L = " << L << ", " << bc << ", s = " << s << " shiftGreen: " << shiftGreen << std::endl;
+					std::cout << "dtau\tAbsMin\tAbsMax\tAbsMean\tRelMin\tRelMax\tRelMean\t" << std::endl;
+					for (uint32_t i = 0; i < dtau_values.size(); ++i) {
+						num dtau = dtau_values[i];
+						uint32_t m = m_values[i];
 
-					// create one instance with checkerboard decomposition and one without
-					// same parameters and same rng seed
+						// create one instance with checkerboard decomposition and one without
+						// same parameters and same rng seed
 
-					ModelParams pars_cb = setupParameters(true, m, dtau, s, bc);
-					RngWrapper rng_cb(5555);
-					typedef DetSDW<false, CB_ASSAAD_BERG> Cb;
-					std::unique_ptr<DetModel> cb_model_ptr = createDetSDW(rng_cb, pars_cb);
-					std::unique_ptr<Cb> sdw_cb = std::unique_ptr<Cb>(dynamic_cast<Cb*>(cb_model_ptr.release()));
+						ModelParams pars_cb = setupParameters(true, m, dtau, s, bc);
+						RngWrapper rng_cb(5555);
+						typedef DetSDW<false, CB_ASSAAD_BERG> Cb;
+						std::unique_ptr<DetModel> cb_model_ptr = createDetSDW(rng_cb, pars_cb);
+						std::unique_ptr<Cb> sdw_cb = std::unique_ptr<Cb>(dynamic_cast<Cb*>(cb_model_ptr.release()));
 
-					ModelParams pars_reg = setupParameters(false, m, dtau, s, bc);
-					RngWrapper rng_reg(5555);
-					typedef DetSDW<false, CB_NONE> Reg;
-					std::unique_ptr<DetModel> reg_model_ptr = createDetSDW(rng_reg, pars_reg);
-					std::unique_ptr<Reg> sdw_reg = std::unique_ptr<Reg>(dynamic_cast<Reg*>(reg_model_ptr.release()));
+						ModelParams pars_reg = setupParameters(false, m, dtau, s, bc);
+						RngWrapper rng_reg(5555);
+						typedef DetSDW<false, CB_NONE> Reg;
+						std::unique_ptr<DetModel> reg_model_ptr = createDetSDW(rng_reg, pars_reg);
+						std::unique_ptr<Reg> sdw_reg = std::unique_ptr<Reg>(dynamic_cast<Reg*>(reg_model_ptr.release()));
 
-					// do two sweeps (one up, one down) for each instance
+						// do two sweeps (one up, one down) for each instance
 
-					sdw_cb->sweepThermalization();
-					sdw_cb->sweepThermalization();
-					sdw_reg->sweepThermalization();
-					sdw_reg->sweepThermalization();
+						sdw_cb->sweepThermalization();
+						sdw_cb->sweepThermalization();
+						sdw_reg->sweepThermalization();
+						sdw_reg->sweepThermalization();
 
-					if (shiftGreen) {
-						sdw_cb->shiftGreenSymmetric();
-						sdw_reg->shiftGreenSymmetric();
-					}
-
-					// compute differences of the Green's functions
-
-					CubeNum g_abs_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green());
-					num max_abs_diff = g_abs_diff.max();
-					num min_abs_diff = g_abs_diff.min();
-					num mean_abs_diff = 0.0;
-					CubeNum g_rel_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green()) / arma::abs(sdw_reg->get_green());
-					num max_rel_diff = g_rel_diff.max();
-					num min_rel_diff = g_rel_diff.min();
-					num mean_rel_diff = 0.0;
-					for (uint32_t k = 1; k <= m; ++k) {
-						num mean_abs_k = 0.0;
-						num mean_rel_k = 0.0;
-						uint32_t elems = g_abs_diff.slice(k).size();
-						for (uint32_t i = 0; i < elems; ++i) {
-							mean_abs_k += g_abs_diff.slice(k)[i];
-							mean_rel_k += g_rel_diff.slice(k)[i];
+						if (shiftGreen) {
+							sdw_cb->shiftGreenSymmetric();
+							sdw_reg->shiftGreenSymmetric();
 						}
-						mean_abs_k /= num(elems);
-						mean_abs_diff += mean_abs_k;
-						mean_rel_k /= num(elems);
-						mean_rel_diff += mean_rel_k;
-					}
-					mean_abs_diff /= num(m);
-					mean_rel_diff /= num(m);
 
-					std::cout << dtau << '\t' << min_abs_diff << '\t' << max_abs_diff << '\t' << mean_abs_diff;
-					std::cout         << '\t' << min_rel_diff << '\t' << max_rel_diff << '\t' << mean_rel_diff << std::endl;
+						// compute differences of the Green's functions
+
+						CubeNum g_abs_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green());
+						num max_abs_diff = g_abs_diff.max();
+						num min_abs_diff = g_abs_diff.min();
+						num mean_abs_diff = 0.0;
+						CubeNum g_rel_diff = arma::abs(sdw_reg->get_green() - sdw_cb->get_green()) / arma::abs(sdw_reg->get_green());
+						num max_rel_diff = g_rel_diff.max();
+						num min_rel_diff = g_rel_diff.min();
+						num mean_rel_diff = 0.0;
+						for (uint32_t k = 1; k <= m; ++k) {
+							num mean_abs_k = 0.0;
+							num mean_rel_k = 0.0;
+							uint32_t elems = g_abs_diff.slice(k).size();
+							for (uint32_t i = 0; i < elems; ++i) {
+								mean_abs_k += g_abs_diff.slice(k)[i];
+								mean_rel_k += g_rel_diff.slice(k)[i];
+							}
+							mean_abs_k /= num(elems);
+							mean_abs_diff += mean_abs_k;
+							mean_rel_k /= num(elems);
+							mean_rel_diff += mean_rel_k;
+						}
+						mean_abs_diff /= num(m);
+						mean_rel_diff /= num(m);
+
+						std::cout << dtau << '\t' << min_abs_diff << '\t' << max_abs_diff << '\t' << mean_abs_diff;
+						std::cout         << '\t' << min_rel_diff << '\t' << max_rel_diff << '\t' << mean_rel_diff << std::endl;
+					}
+					std::cout << std::endl;
 				}
-				std::cout << std::endl;
 			}
 		}
 	}
