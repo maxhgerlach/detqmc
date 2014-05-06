ae9e648f9648f96d691d1e6aba178594420e9044
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2014-05-06 19:48:36 +0200

    files rescued from branch sdw-moreobs

---
+++ old/sdw-moreobs_symmat.h
@@ -0,0 +1,62 @@
+/*
+ * symmat.h
+ *
+ *  Created on: Oct 26, 2013
+ *      Author: max
+ */
+
+
+// struct holding data that can be indexed via 2 dimensions, exchange of the two
+// indices should yield the same data --> SymMat
+
+// variation without entries for idx1==idx2 --> SymMatOffdiag
+
+#include "checkarray.h"
+#include <utility>
+#include <stdexcept>
+
+template<typename T, uint32_t size>
+struct SymMat {
+	static const uint32_t dataSize = size * (size + 1) / 2;
+
+	checkarray<T, dataSize> data;
+
+	T& operator()(uint32_t idx1, uint32_t idx2) {
+#ifdef _GLIBCXX_DEBUG
+		if (idx2 >= size or idx1 >= size) {
+			throw std::out_of_range("SymMat: access outside matrix");
+		}
+#endif //_GLIBCXX_DEBUG
+		if (idx2 < idx1) {
+			std::swap(idx1, idx2);
+		}
+		// computation of dataIndex (0-based like everything):
+		// dataIndex = size + (size-1) + ... + (size-idx1+1) + idx2 - idx1
+		//           = idx1 * size - \sum_{i=1}^{idx1-1} i   + idx2 - idx1
+		//           = idx1 * size - (idx1-1)*idx1/2 + idx2 - idx1
+		return data[idx1 * size - (idx1-1)*idx1/2 + idx2 - idx1];
+	}
+};
+
+
+template<typename T, uint32_t size>
+struct SymMatOffdiag {
+	static const uint32_t dataSize = size * (size + 1) / 2 - size;  // subtract diagonal length
+
+	checkarray<T, dataSize> data;
+
+	T& operator()(uint32_t idx1, uint32_t idx2) {
+#ifdef _GLIBCXX_DEBUG
+		if (idx2 == idx1) {
+			throw std::out_of_range("SymMatOffdiag: access to diagonal elements");
+		}
+		if (idx2 >= size or idx1 >= size) {
+			throw std::out_of_range("SymMatOffdiag: access outside matrix");
+		}
+#endif //_GLIBCXX_DEBUG
+		if (idx2 < idx1) {
+			std::swap(idx1, idx2);
+		}
+		return data[idx1 * size - (idx1-1)*idx1/2 + idx2 - idx1 - (idx1 + 1)]; // subtract number of elements that would be on diagonal
+	}
+};
