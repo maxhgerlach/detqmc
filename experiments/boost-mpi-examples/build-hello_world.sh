7b5faa1e495e1d01b831aa33b394e4734e6e3a4c
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2014-06-11 15:48:10 +0200

    boost mpi examples

---
+++ experiments/boost-mpi-examples/build-hello_world.sh
@@ -0,0 +1,1 @@
+mpicxx -std=c++0x -I~/qmc/det/src hello_world.cpp ../../boost-mpi/*.cpp ../../boost-serialization/*.cpp  -o hello_world
---
+++ experiments/boost-mpi-examples/build-hello_world_broadcast.sh
@@ -0,0 +1,1 @@
+mpicxx -std=c++0x -I~/qmc/det/src hello_world_broadcast.cpp ../../boost-mpi/*.cpp ../../boost-serialization/*.cpp  -o hello_world_broadcast
