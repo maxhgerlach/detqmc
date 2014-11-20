7742d12c7f8a0496cab2ede5cbe3d344d87d5475
Max H. Gerlach <gerlach@thp.uni-koeln.de>
2014-11-20 11:36:38 +0100

    archived some old code

--- detsdwopdim.h
+++ detsdwopdim.h
@@ -162,26 +162,7 @@ protected:
         subv = MatCpx(realPart, imagPart);
     }
     
-    // template<class Matrix>
-    // static void setReal(arma::subview<num> subv, Matrix realPart) {
-    //     subv = realPart;
-    // }
-    // template<class Matrix>
-    // static void setReal(arma::subview<cpx> subv, Matrix realPart) {
-    //     //warning: unnecessarily slow
-    //     MatCpx temp_mat = subv;
-    //     temp_mat.set_real(
-    //     subv.set_real(realPart);
-    // }
-    // template<class Matrix>
-    // static void setImag(arma::subview<num> subv, Matrix imagPart) {
-    //     subv = imagPart;
-    // }
-    // template<class Matrix>
-    // static void setImag(arma::subview<cpx> subv, Matrix imagPart) {
-    //     subv.set_imag(imagPart);
-    // }
-    
+   
     
 /*
 
